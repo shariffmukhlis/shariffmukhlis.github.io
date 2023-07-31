c2c2ccd033472304fed30e5b1fd7a6310a77d57b
Shariff Mukhlis <shariffmukhlis0@gmail.com>
2023-08-01 02:03:30 +0800

    v2.1 - finish main page

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-//hamburger menu on click add class active
-document.querySelector('.hamburgermenu').addEventListener('click', function() {
-  document.querySelector('.hamburgermenu').classList.toggle('active');
-  document.querySelector('.menusmall').classList.toggle('active');
+const faders = document.querySelectorAll('.fade-in');
+const sectiontitle = document.querySelectorAll('.section-title');
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px" // Adjust this value to control when the fade-in effect starts
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('appear');
+        } else {
+            entry.target.classList.remove('appear');
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
 });
